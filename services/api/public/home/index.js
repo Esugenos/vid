398a36a0cf0e3f1b6dd00d9d622dd78c19c77806
Esugenos <r1.bernier@orange.fr>
2021-03-18 15:08:33 +0100

    Some front draft

---
+++ services/api/public/home/index.js
@@ -0,0 +1,8 @@
+const main = document.getElementById("main");
+
+const hcount = 64, vcount = 36;
+main.addEventListener('mousemove', e => {
+    const x = e.offsetX, y = e.offsetY;
+    const { width, height } = e.target.getBoundingClientRect();
+    
+});
