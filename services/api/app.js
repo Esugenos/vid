1d401cf9aa9829a40cfcd3c4e4cda739294db681
Ebernn <r1.bernier@orange.fr>
2021-04-04 15:33:47 +0200

    Updated docker-compose + nodemon for HMR

--- services/api/app.js
+++ services/api/app.js
@@ -5,10 +5,10 @@ const { generateVideo } = require('./utils');
 const app = express();
 const port = 8000;
 
-app.use('/static', express.static('/public'));
+app.use('/static', express.static('./public'));
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
 
-generateVideo('/public/original.mp4', '/public/ntsh.mp4');
+generateVideo('./public/original.mp4', './public/ntsh.mp4');
